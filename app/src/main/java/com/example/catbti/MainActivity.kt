303d4939e953fdee69238a70484361b9314f255e
infobank <infobank@soohwan-IBN1-CR.infobank.net>
2023-07-28 11:23:44 +0900

    main ã화면ã

--- app/src/main/java/com/example/catbti/MainActivity.kt
+++ app/src/main/java/com/example/catbti/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.DividerDefaults.color
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.catbti.ui.main.Main
+import com.example.catbti.ui.theme.Background
 import com.example.catbti.ui.theme.CatBTITheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +21,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CatBTITheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Main()
                 }
             }
         }
