0251a5ee8d36fd3b0882dc3ca986c3887483818f
infobank <infobank@soohwan-IBN1-CR.infobank.net>
2023-07-31 17:30:37 +0900

    Test Complete Button

--- app/src/main/java/com/example/catbti/ui/test/QuestionList.kt
+++ app/src/main/java/com/example/catbti/ui/test/QuestionList.kt
@@ -1,16 +1,24 @@
 package com.example.catbti.ui.test
 
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.catbti.Questions
+import com.example.catbti.R
 import com.example.catbti.data.test.Question
+import com.example.catbti.ui.main.MainButton
+import com.example.catbti.ui.theme.Background
 import com.example.catbti.ui.theme.CatBTITheme
 
 
@@ -19,13 +27,21 @@ fun QuestionList(
     list: List<Question>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier) {
+    LazyColumn(
+        modifier = modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         items(
             items = list,
             key = { question -> question.question }
         ) { question ->
             QuestionItem(question = question.question)
         }
+        item() {
+            MainButton(text = stringResource(R.string.complete_test_button), textSize = 20)
+        }
+
+
     }
 }
 
@@ -35,7 +51,7 @@ fun PPPairedListPreview() {
     CatBTITheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = Background
         ) {
             QuestionList(getQuestion())
         }
