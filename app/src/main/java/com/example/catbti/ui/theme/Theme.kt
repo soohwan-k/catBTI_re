953c9476cd442527c560d072681e1ae776d4199e
infobank <infobank@soohwan-IBN1-CR.infobank.net>
2023-08-08 15:49:03 +0900

    StatusBar Color Change

--- app/src/main/java/com/example/catbti/ui/theme/Theme.kt
+++ app/src/main/java/com/example/catbti/ui/theme/Theme.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
@@ -18,11 +19,11 @@ import androidx.core.view.WindowCompat
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Background,
     secondary = PurpleGrey40,
     tertiary = Pink40,
     background = Background
@@ -59,7 +60,7 @@ fun CatBTITheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = true
         }
     }
 
